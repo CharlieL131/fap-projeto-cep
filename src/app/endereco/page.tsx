fe4d731408f78f09b34c0dcef32ae468267a9d3a
Charlie131 <charlieeelages@gmail.com>
2024-11-22 01:18:13 -0300

    feat: endereco

--- src/app/endereco/page.tsx
+++ src/app/endereco/page.tsx
@@ -1,14 +1,22 @@
+import "./styles.css"
 import Header from "@/components/header";
 import Tab from "@/components/tab";
+import Hill from "@/components/hill";
+import Brazil from "@/components/brazil";
 
-export default function Home() {
+export default function Endereco() {
   return (
-    <div className="">
+    <div className="page">
       <Header>
         <Tab route="/" name="Home"></Tab>
         <Tab route="/endereco" name="Endereço"></Tab>
         <Tab route="/cep" name="CEP"></Tab>
       </Header>
+      <div style={{flex: "1", position: "relative"}}>
+      
+      </div>
+      <Brazil/>
+      <Hill/>
     </div>
   );
 }
