3b095d50fb17878331b0c1d425ef2fce41b69a7d
Charlie131 <charlieeelages@gmail.com>
2024-11-22 01:15:03 -0300

    feat: brazil

---
+++ src/components/brazil/index.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import "./styles.css"
+
+export default function Brazil() {
+    return (
+        <Image alt="" src={"/brazil.svg"} width={800} height={800} className="brazil" ></Image>
+    )
+}
